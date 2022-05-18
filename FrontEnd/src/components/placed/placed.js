b29a19891c0fa6d447389561fab0acd09e09e24c
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-05-18 19:38:51 +0530

    backend updated

--- FrontEnd/src/components/placed/placed.js
+++ FrontEnd/src/components/placed/placed.js
@@ -3,62 +3,100 @@ import Deliverysvg from './Deliverysvg'
 import './placed.css'
 
 function placed() {
-  return (
-    <div class="d-md-flex h-md-100">
+    return (
+        <div class="d-md-flex h-md-100">
 
-    <div class="col-md-6 p-0 bg-indigo h-md-100">
-        <div class="text-white d-md-flex h-100 p-5">
-            <div class="logoarea pt-5 pb-5">
+            <div class="col-md-6 p-0 bg-indigo h-md-100">
+                <div class="text-dark d-md-flex h-100 p-2">
+                    <div class="logoarea pt-5 pb-5">
 
 
-            <div className="svg d-flex justify-content-center">
-			<div className="hclass mt-3">
-      <h1>Order <d> Placed </d>!</h1>
-      <p>Order will recived as soon as possible<br/>
-      within two or three working days.
-      </p>
-      <p style={{color: '#2949ff'}}>Thanks for deaing with us :)</p>
-      </div>
-              
+                        <div className="d-flex justify-content-center font-weight-bold">
+                            <div className="hclass mt-3">
+                                <h1>Order <d style={{ color: '#11ad00' }}> Placed </d>!</h1>
+                                <p>Order will recived as soon as possible<br />
+                                    within two or three working days.
+                                </p>
+                                <p style={{ color: '#2949ff' }}>Thanks for deaing with us :)</p>
+                            </div>
+                        </div>
+                        <div className="mb-2 p-2">
+                            <Deliverysvg
+                                height={600}
+                                width={600}
+                            />
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <Deliverysvg
-                height={500}
-                width={500}
-              />
+
+
+
+            <div class="d-md-flex ml-3" id="hclass">
+
+
+                <div class="row row-cols-1">
+                    <div class="col">                <div className="card shadow" id="card">
+                        <div className="card-body mb-5 ml-2 mr-2">
+                            <div class="mb-3 bg-white">
+                                <div class="bs-example">
+                                    <div class="d-flex justify-content-between">
+                                        <div><h3>CITY MEDICALS</h3></div>
+                                        <div><button type="button" class="btn btn-outline-dark"><i class="fa fa-cloud-download" aria-hidden="true"></i></button></div>
+                                    </div>
+
+                                </div>
+                                <h6 className="text-muted">Online Payment Receipt</h6><br />
+                                <p className="card-text">Receipt Number : {Number}</p>
+                                <p className="card-text">Client Name :  {Number}</p>
+                                <div class="accordion-item ">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                           Check Your Items :
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <ol>
+                                                <li>{Number}</li>
+                                                <li>{Number}</li>
+                                                <li>{Number}</li>
+                                                <li>{Number}</li>
+                                                <li>{Number}</li>
+                                                <li>{Number}</li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div class="alert alert-success d-flex align-items-bottom fade show positon-absolute">
+                                <i class="fa fa-certificate" aria-hidden="true"></i>
+                                <strong class="mx-2"></strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                <button type="button" class="btn-close ml-2" data-bs-dismiss="alert"></button>
+
+                            </div>
+                            <button class="btn btn-primary align-content-right" type="button" id="btnnext">Button</button>
+                        </div>
+
+
+                    </div></div>
+                    <div id="btnright">
+                       
+                        
+                    </div>
+                </div>
+
+
+
 
             </div>
+
         </div>
-    </div>
 
-     
-    <div class="col-md-6 p-0 bg-white loginarea">
-        <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
-
-
-        <div className="card bg-light shadow bg-white rounded mt-5" style={{border: '18rem;'}}>
-        <div className="card-body">
-    <h5 className="card-title">CITY MEDICALS</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Online Payment Receipt</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <p className="card-text">Receipt ID : .............</p>
-    <p className="card-text">Client ID : ..............</p>
-    <p className="card-text">Items: </p>
-        <ul>
-            <li>ttt</li>
-            <li>ttt</li>
-            &nbsp;&nbsp; more
-        </ul>
-    <button href="/" className="card-link btn btn-danger">Another link</button>
-    <button href="/" className="card-link btn btn-primary">Another link</button>
-  </div>
-  </div>
-
-            
-        </div>
-    </div>
-        
-</div>
-)
+    )
 }
 
-export default placed
+export default placed;
